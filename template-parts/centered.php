65924ab6723c91cc072645085c4862c3293a07c2
Henry puttick <hputtick@me.com>
2019-05-28 22:24:12 +0700

    additional file markup for extra content template

--- template-parts/centered.php
+++ template-parts/centered.php
@@ -1,5 +1,6 @@
 <?php
 $show           = get_field('show_info');
+$file           = get_field('section_button_file');
 ?>
 
 <?php if ($show == 1) { ?>
@@ -20,12 +21,25 @@ $show           = get_field('show_info');
                 <?php $program = get_field('section_button_link'); ?>
 
                 <?php if ($program == 2) { ?>
+                    <div class="has-text-centered block">
                     <div data-aos="fade-left" class="field is-grouped is-grouped-centered">
                         <a href="<?php the_field('section_button'); ?>" class="button is-dark is-outlined"><?php the_field('section_button_name'); ?></span></a>
                     </div>
+                    </div>
                 <?php } else if ($program  == 3) { ?>
+                    <div class="has-text-centered block">
                     <div data-aos="fade-left" class="field is-grouped is-grouped-centered">
                         <a href="<?php the_field('section_button_url'); ?>" class="button is-dark is-outlined"><?php the_field('section_button_name'); ?></span></a>
+                    </div>
+                    </div>
+                <?php } else if ($program  == 4) { ?>
+                    <div data-aos="fade-left" class="has-text-centered block">
+                    <div class="field is-grouped is-grouped-centered">
+                        <a target="_blank" href="<?php echo $file['url'] ?>" class="button is-primary is-outlined is-file">
+                            <span class="icon is-small"><i class="fas fa-file-pdf"></i></span>
+                            <span><?php the_field('section_button_name'); ?></span>
+                        </a>
+                    </div>
                     </div>
                 <?php } ?>
             </div>
