2890618fdb23e612880a5db4290c0dbc0695c40e
Henry puttick <hputtick@me.com>
2019-06-30 12:37:37 +0700

    functions for search

--- functions.php
+++ functions.php
@@ -806,3 +806,19 @@ include_once(get_stylesheet_directory() . '/acf/acf.php');
 	}
 	 // Add to the admin_init hook of your theme functions.php file 
 	add_action( 'init', 'myplugin_settings' );
+
+	function fb_filter_query( $query, $error = true ) {
+	
+		if ( is_search() ) {
+			$query->is_search = false;
+			$query->query_vars[s] = false;
+			$query->query[s] = false;
+			
+			// to error
+			if ( $error == true )
+				$query->is_404 = true;
+		}
+	}
+	
+	add_action( 'parse_query', 'fb_filter_query' );
+	add_filter( 'get_search_form', create_function( '$a', "return null;" ) );
