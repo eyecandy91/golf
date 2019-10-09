50917fab243879ea2f6d89a32510400b8185a31b
Henry puttick <hputtick@me.com>
2019-10-09 13:07:35 +0700

    Christmas templates

---
+++ page-templates/xmas.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying all pages
+ * 
+ * Template Name: Christmas page
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package _s
+ */
+
+get_header();
+
+get_template_part( 'template-parts/hero' ); 
+get_template_part( 'template-parts/centered' ); 
+get_template_part( 'template-parts/content-split-1'); 
+get_template_part( 'template-parts/performance_xmas' ); 
+// get_template_part( 'template-parts/rules' ); 
+
+
+// get_sidebar();
+get_footer();
---
+++ template-parts/performance_xmas.php
@@ -0,0 +1,89 @@
+<?php
+$additonal          = get_field('additional_header');
+?>
+
+<?php if (!empty($additonal)) : ?>
+    <section class="section content">
+        <div class="container">
+            <h5 data-aos="fade-down" class="title is-2 is-size-3-mobile fade-in is-marginless has-text-centered">
+                <?php echo $additonal ?>
+            </h5>
+        </div>
+    </section>
+<?php endif; ?>
+
+<section class="has-text-centered section content has-background-grey-lighter positve-section small">
+    <div class="container">
+        <div class="columns is-multiline negative-section small">
+
+            <?php
+
+            $args = array(
+                'posts_per_page'     => -1,
+                'category_name'     => 'christmas',
+                'post_type'         => 'christmas',
+            );
+            // $months = array(
+            //     'January',
+            //     'February',
+            //     'March',
+            //     'April',
+            //     'may',
+            //     'June',
+            //     'July ',
+            //     'August',
+            //     'September',
+            //     'October',
+            //     'November',
+            //     'December',
+            // );
+            $items = get_posts($args);
+            foreach ($items as $post) : setup_postdata($post);
+                $count++; //increment the variable by 1 each time the loop executes
+
+                $date               = get_field('date_picker');
+                $performer          = get_field('performer');
+                $xtra               = get_field('info_performer');
+                $perform_file       = get_field('perform_file');
+                ?>
+
+                <div data-aos="fade-right" class="column is-full-mobile is-one-third-tablet is-one-quarter-desktop ">
+                    <div class="has-background-white box is-primary is-tooltip">
+
+                        <h5 class="title is-alternative has-text-weight-bold is-5 is-uppercase">
+
+                            <?php if ($date) :
+                                echo $date;
+                            endif; ?>
+
+                        </h5>
+                        <div>
+                            <?php if ($performer) :
+                                echo "<div class='block is-marginless'>";
+                                echo $performer;
+                                echo "</div>";
+                            endif; ?>
+                            <?php if ($xtra) : ?>
+                                <div class="tooltip is-tooltip-multiline is-tooltip-left-mobile" data-tooltip="<?php echo $xtra; ?>">
+                                    <span><i class="far fa-question-circle"></i></span>
+                                    <span></span>
+                                </div>
+                            <?php endif; ?>
+                            <?php if ($perform_file) : ?>
+                                <div>
+                                    <a href="<?php echo $perform_file['url'] ?>" target="_blank" class="button is-dark is-small is-file">
+                                        <span class="icon is-small"><i class="fas fa-file-pdf"></i></span>
+                                        <span>click to view more</span>
+                                    </a>
+                                </div>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                </div>
+
+            <?php endforeach;
+        wp_reset_postdata(); ?>
+
+        </div>
+    </div>
+</section>
